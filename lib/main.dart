55728ab0a4e996e38c46081694d0a4c5ea9688aa
nostreet <nostreet1@gmail.com>
2021-03-19 18:57:31 +1300

    added a few questions for testing

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,43 @@ void main() {
   // calls the build widget
   runApp(MyFollowMeApp());
 // also possible: void main() => runApp(MyFollowMeApp());
-
 }
+
 // StatelessWidget does the heavy lifting features for us from the
 // material.dart app
-class MyFollowMeApp extends StatelessWidget{
+class MyFollowMeApp extends StatelessWidget {
+  var questions = [
+    "What's your favorite color?",
+    "What's your favorite animal?",
+  ];
 
   @override
   Widget build(BuildContext context) {
     // named argument to easier remember which argument goes
     // into what position
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text("My Follow Me App"),
-      ) ,
-      body: Text("This is my default text!"),
-    ),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Follow Me App"),
+        ),
+        body: Column(
+          children: [
+            Text('The questions:'),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
